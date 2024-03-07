7b3392bd977d75dfeca2001f6f9da5d73ec84a0f
rxlyn <rxlyn03@gmail.com>
2024-03-07 02:44:34 -0500

    Added REST GET command mapping to catalogue controller

---
+++ backend/src/main/java/ecommerce/controller/CatalogueResultFilter.java
@@ -0,0 +1,42 @@
+package ecommerce.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ecommerce.entity.Product;
+import ecommerce.exception.AuctionNotFoundException;
+import ecommerce.exception.ProductNotFoundException;
+
+public class CatalogueResultFilter {
+	public static List<Product> filterProductByType(List<Product> productList, String type){
+		List<Product> tmp = new ArrayList<Product>();
+		List<Product> full = productList;
+		for (int i = 0; i < full.size(); i++) {
+			if (full.get(i).getAuctionType().equals(type)) {
+				tmp.add(full.get(i));
+			}
+		}
+		if (tmp.size() < 1) {
+			throw new AuctionNotFoundException(type);
+		}
+		else {
+			return tmp;
+		}
+	}
+	
+	public static List<Product> filterProductByName(List<Product> productList, String name){
+		List<Product> tmp = new ArrayList<Product>();
+		List<Product> full = productList;
+		for (int i = 0; i < full.size(); i++) {
+			if (full.get(i).getName().contains(name)) {
+				tmp.add(full.get(i));
+			}
+		}
+		if (tmp.size() < 1) {
+			throw new ProductNotFoundException(name);
+		}
+		else {
+			return tmp;
+		}
+	}
+}
---
+++ backend/src/main/java/ecommerce/exception/AuctionNotFoundException.java
@@ -0,0 +1,7 @@
+package ecommerce.exception;
+
+public class AuctionNotFoundException extends RuntimeException{
+	public AuctionNotFoundException(String type) {
+		super("Count not find any products with auction type: " + type);
+	}
+}
