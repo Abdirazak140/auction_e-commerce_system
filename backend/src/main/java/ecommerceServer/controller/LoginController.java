30172a5177eaa987b61b0c4c3e6293901568d8b4
Abdirazak Yusuf <abdirazak140@gmail.com>
2024-03-09 15:34:04 -0500

    Added getAuthState

--- backend/src/main/java/ecommerceServer/controller/LoginController.java
+++ backend/src/main/java/ecommerceServer/controller/LoginController.java
@@ -5,37 +5,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-import org.springframework.ui.Model;
 import ecommerceServer.connection.LoginRequest;
 import ecommerceServer.service.AuthenticationMessage;
 import ecommerceServer.service.LoginService;
+import jakarta.servlet.http.HttpSession;
 
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/users")
-@SessionAttributes("auth")
 public class LoginController {
 	
 	@Autowired
 	private LoginService loginService;
 	
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, Model model){
+	public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, HttpSession httpSession){
 		String username = loginRequest.getUsername();
 		String password = loginRequest.getPassword();
 		
 		AuthenticationMessage result = loginService.login(username, password);
 		
 		if (result.isState()) {
-			model.addAttribute("auth", true);
+			httpSession.setAttribute("auth", Boolean.TRUE);
 			return ResponseEntity.ok(result.getMsg());
 		}
 		else {
@@ -44,10 +40,16 @@ public class LoginController {
 	}
 	
 	@GetMapping("/getAuthState")
-	public ResponseEntity<Boolean> getAuthState(@ModelAttribute("auth") Boolean state){
-		if (state == null) {
+	public ResponseEntity<Boolean> getAuthState(HttpSession httpSession){
+		if (httpSession.getAttribute("auth") == null) {
 			return ResponseEntity.ok(false);
 		}
-		return ResponseEntity.ok(state);
+		return ResponseEntity.ok(true);
+	}
+	
+	@GetMapping("/logout")
+	public ResponseEntity<String> logout(HttpSession httpSession){
+		httpSession.setAttribute("auth", Boolean.FALSE);
+		return ResponseEntity.ok("Successful");
 	}
 }
