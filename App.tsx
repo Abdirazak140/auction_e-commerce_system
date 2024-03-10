f13879a092fdf75d0a36a5fd9fcf629e54585b4a
Hassan Dannyal <100085970+Spectra-Ph@users.noreply.github.com>
2024-03-09 20:18:28 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Payment.css';
+import Header from './Header';
+import { Link, useLocation } from 'react-router-dom'
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+    < Header/>
+      <div className='contents'>
+      <div className="left">
+      <h1>Winning Bidder</h1>
+        <div className='Winning-bidder'>
+          <p>First Name: XXX</p>
+          <p>Last Name: XXX</p>
+          <p>Street: XXX</p>
+          <p>Number: XXX</p>
+          <p>Province: XXX</p>
+          <p>Country: XXX</p>
+          <p>Postal Code: XXX</p>
+          <p>Total Cost: XXX</p>
+        </div>
+      </div>
+      <hr /> {/* Horizontal line */}
+      <div className="right">
+        <div className='Credit'>
+          <h1>Card Details</h1>
+          <p>Card Number</p>
+          <input></input>
+          <p>Name on Card</p>
+          <input></input>
+          <p>Expiry Date</p>
+          <input></input>
+          <p>Security Code</p>
+          <input></input>
+          <Link to="/Receipt" className='submit'>SUBMIT</Link>
+          </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
